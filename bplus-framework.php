7678652c1cbb4a817fd40a33fdb7ed102fe038ed
Luis Abarca <luisabarca@gmail.com>
2017-04-21 01:38:08 -0500

    Added auto-update information

--- bplus-framework.php
+++ bplus-framework.php
@@ -23,6 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       bplus-framework
  * Domain Path:       /languages
+ * Bitbucket Plugin URI: https://bitbucket.org/bplusmx/bplus-framework
  */
 
 // If this file is called directly, abort.
