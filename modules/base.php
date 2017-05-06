e7ed2d8b9db52f21856bdcea2e9ec11211bbaada
Luis Abarca <luisabarca@gmail.com>
2017-05-06 01:19:39 -0500

    Cleanup headers moved to another file

--- modules/base.php
+++ modules/base.php
@@ -9,7 +9,4 @@
  * @subpackage Bplus_Framework/modules
  */
 
-// Remueve la etiqueta del framework alkivia 0.8 !
-remove_action( 'wp_head', '_ak_framework_meta_tags' );
-remove_action( 'wp_head', 'wp_generator' );
-remove_action( 'wp_head', 'shrsb_add_ogtags_head' );
+include 'cleanup.php';
